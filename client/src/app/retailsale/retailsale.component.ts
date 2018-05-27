9ee30cbbc36ed792125930a825950915f9d3a449
syedzainhasan <syedzainhasan@hotmail.com>
2018-05-27 12:27:16 +0500

    retail front end

--- client/src/app/retailsale/retailsale.component.ts
+++ client/src/app/retailsale/retailsale.component.ts
@@ -143,6 +143,7 @@ addFieldValue() {
 this.newAttribute.quantity = this.retailsale.get('Quantity').value;
 this.newAttribute.price = this.retailsale.get('Price').value;
 this.newAttribute.discount = this.retailsale.get('Discount').value;
+this.newAttribute.gst=this.retailsale.get('GST').value;
 
 
 this.fieldArray.push(this.newAttribute);
@@ -159,7 +160,7 @@ for(this.i=0;this.i<this.fieldArray.length;this.i++)
   
 this.getDATA= Number(this.fieldArray[this.i].price);
 this.price = this.price+ (this.getDATA);
-this.getGST=parseFloat(this.retailsale.get('GST').value);
+this.getGST=parseFloat(this.fieldArray[this.i].gst);
 this.getDISCOUNT = Number(this.fieldArray[this.i].discount);
 this.getDISCOUNT=this.getDISCOUNT/100;
 this.getDISCOUNT=this.getDISCOUNT*this.getDATA;
@@ -180,11 +181,11 @@ this.discount=this.discount+this.getDISCOUNT;
 
 
 }
+
+
+
+
 this.net = this.price-this.discount+this.gst;
-
-
-
-
 
 
 }
@@ -192,15 +193,27 @@ this.net = this.price-this.discount+this.gst;
 deleteFieldValue(index) {
   
   this.fieldArray.splice(index, 1);
+  if(this.fieldArray.length==0)
+  {
+ 
+ this.net=0;
+ this.price=0;
+ this.discount=0;
+ this.gst=0;
+  }
 }
 deleteFieldValues(index) {
-  this.fieldArray=[];
-  this.newAttribute.code = "";
-  this.newAttribute.product = "";
+this.fieldArray=[];
+this.newAttribute.code = "";
+this.newAttribute.product = "";
 this.newAttribute.quantity = "";
 this.newAttribute.price = "";
 this.newAttribute.discount ="";
- 
+
+ this.net=0;
+ this.price=0;
+ this.discount=0;
+ this.gst=0;
 }
 
 }
